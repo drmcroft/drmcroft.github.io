5d5eaa1895bc8dfb029037366c19664f3c741c18
drmcroft <pointy.maxo@gmail.com>
2015-09-19 20:05:43 -0400

    new css, index, and js

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -29,3 +29,19 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
        window.location.hash = hash; 
      }); 
 }); 
+
+$("#back-to-top a[href^='#']").on('click', function(e) { 
+   // prevent default anchor click behavior 
+   e.preventDefault(); 
+   // store hash 
+   var hash = this.hash; 
+   // animate 
+   $('html, body').animate({ 
+       scrollTop: $(this.hash).offset().top -50
+     }, 300, function(){ 
+
+       // when done, add hash to url 
+       // (default click behaviour) 
+       window.location.hash = hash; 
+     }); 
+}); 
